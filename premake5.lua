28f930c4a5fbe2bdaf5317b8448ab58165d36e01
Thomas Dela Pena <tdelapena20@gmail.com>
2020-01-25 14:41:32 -0600

    fixed premake file

--- premake5.lua
+++ premake5.lua
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-project "ImGui"
-    kind "StaticLib"
-    language "C++"
-	cppdialect "C++17"
-	staticruntime "on"
-
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-    files
-    {
-    "imconfig.h",
-    "imgui.h",
-    "imgui.cpp",
-    "imgui_draw.cpp",
-    "imgui_internal.h",
-    "imgui_widgets.cpp",
-    "imstb_rectpack.h",
-    "imstb_textedit.h",
-    "imstb_truetype.h",
-    "imgui_demo.cpp"
-    }
-
-    filter "system:windows"
-    systemversion "latest"
-
-    filter "system:linux"
-    systemversion "latest"
-    pic "on"
-
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
-	
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
-=======
 project "ImGui"
     kind "StaticLib"
     language "C++"
@@ -59,16 +20,20 @@ project "ImGui"
     }
 
     filter "system:windows"
-    systemversion "latest"
-    cppdialect "C++17"
-    staticruntime "On"
+        systemversion "latest"
+        cppdialect "C++17"
+        staticruntime "On"
 
     filter "system:linux"
-    systemversion "latest"
-    cppdialect "C++17"
-    staticruntime "On"
-    pic "On"
+        systemversion "latest"
+        cppdialect "C++17"
+        staticruntime "On"
+        pic "On"
 
-    filter { "system:windows", "configurations:Release" }
-    buildoptions "/MT"
->>>>>>> 2bb1071cedd39804cd83422582f397f57b626f67
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
